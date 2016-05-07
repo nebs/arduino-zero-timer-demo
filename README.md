8bba6c3d07c35097c2f11ca1817647ad92169f4c
Nebojsa Petrovic <nebspetrovic@gmail.com>
2016-05-06 21:38:29 -0700

    Fist commit
    This commit introduces the basic project. Unfortunately most of the
    history of these changes is lost because this project was ripped out of
    another project and simplified to expose the timer specifically.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Arduino Zero Timer Demo
+This is a simple project to demonstrate smoothly changing the timer's frequency in real time.
+
+## Setup
+1. Install ([PlatformIO](http://platformio.org)).
+2. Run `make upload` to upload.
---
+++ src/main.cpp
@@ -0,0 +1,95 @@
+#include "Arduino.h"
+
+#define LED_PIN 2
+#define POT_PIN 2
+
+#define POT_JITTER_THRESHOLD 10
+#define CPU_HZ 48000000
+#define TIMER_PRESCALER_DIV 1024
+
+void startTimer(int frequencyHz);
+void setTimerFrequency(int frequencyHz);
+int frequencyFromPotValue(int potValue);
+void TC3_Handler();
+
+int potValue = 0;
+bool isLEDOn = false;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  potValue = analogRead(POT_PIN);
+  startTimer(frequencyFromPotValue(potValue));
+}
+
+void loop() {
+  int newPotValue = analogRead(POT_PIN);
+  // We only want to update the frequency if there has been significant changes
+  // to the potentiometer. We do this to prevent jitters but also to make this
+  // loop more efficient.
+  if (abs(newPotValue - potValue) > POT_JITTER_THRESHOLD) {
+    potValue = newPotValue;
+    setTimerFrequency(frequencyFromPotValue(potValue));
+  }
+}
+
+int frequencyFromPotValue(int potValue) {
+  // potValue is between 0 and 1023 so we scale it to 1Hz -> 34Hz
+  return (potValue / 30) + 1;
+}
+
+void setTimerFrequency(int frequencyHz) {
+  int compareValue = (CPU_HZ / (TIMER_PRESCALER_DIV * frequencyHz)) - 1;
+  TcCount16* TC = (TcCount16*) TC3;
+  while (TC->STATUS.bit.SYNCBUSY == 1);
+  // Make sure the count is in a proportional position to where it was
+  // to prevent any jitter or disconnect when changing the compare value.
+  TC->COUNT.reg = map(TC->COUNT.reg, 0, TC->CC[0].reg, 0, compareValue);
+  TC->CC[0].reg = compareValue;
+}
+
+/*
+This is a slightly modified version of the timer setup found at:
+https://github.com/maxbader/arduino_tools
+ */
+void startTimer(int frequencyHz) {
+  REG_GCLK_CLKCTRL = (uint16_t) (GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_ID (GCM_TCC2_TC3)) ;
+
+  TcCount16* TC = (TcCount16*) TC3;
+
+  while ( GCLK->STATUS.bit.SYNCBUSY == 1 );
+  TC->CTRLA.reg &= ~TC_CTRLA_ENABLE;
+
+  // Use the 16-bit timer
+  while (TC->STATUS.bit.SYNCBUSY == 1);
+  TC->CTRLA.reg |= TC_CTRLA_MODE_COUNT16;
+
+  // Use match mode so that the timer counter resets when the count matches the compare register
+  while (TC->STATUS.bit.SYNCBUSY == 1);
+  TC->CTRLA.reg |= TC_CTRLA_WAVEGEN_MFRQ;
+
+  // Set prescaler to 1024
+  while (TC->STATUS.bit.SYNCBUSY == 1);
+  TC->CTRLA.reg |= TC_CTRLA_PRESCALER_DIV1024;
+
+  setTimerFrequency(frequencyHz);
+
+  // Enable the compare interrupt
+  TC->INTENSET.reg = 0;
+  TC->INTENSET.bit.MC0 = 1;
+
+  NVIC_EnableIRQ(TC3_IRQn);
+
+  while (TC->STATUS.bit.SYNCBUSY == 1);
+  TC->CTRLA.reg |= TC_CTRLA_ENABLE;
+}
+
+void TC3_Handler() {
+  TcCount16* TC = (TcCount16*) TC3;
+  // If this interrupt is due to the compare register matching the timer count
+  // we toggle the LED.
+  if (TC->INTFLAG.bit.MC0 == 1) {
+    TC->INTFLAG.bit.MC0 = 1;
+    digitalWrite(LED_PIN, isLEDOn);
+    isLEDOn = !isLEDOn;
+  }
+}
